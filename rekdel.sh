876ea4180003f8d89ad3215c732be922e49705a4
krewrom <roman.krewer@deutsche-boerse.com>
2016-10-09 13:09:23 +0200

    added more escaping

--- rekdel.sh
+++ rekdel.sh
@@ -1,3 +1,7 @@
 export IFS=$'\n'
-for m4afile in $(find -iname "*.m4a"); do echo "rm '$m4afile'" | sed 's/.m4a/.m4p/g'; done > doit.sh
+for m4afile in $(find -iname "*.m4a")
+do
+ m4pfile=$(printf '%q' "$m4afile" | sed 's/.m4a/.m4p/g')
+ echo "rm $m4pfile"
+done > doit.sh
 chmod +x doit.sh
--- test/doit.sh
+++
@@ -1,4 +0,0 @@
-rm './a/ab2.m4p'
-rm './a/xyz.m4p'
-rm './a/abl.m4p'
-rm './a/with space.m4p'
